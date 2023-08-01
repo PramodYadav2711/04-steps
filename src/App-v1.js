e6551965fbb7237b359cf5e09df8c8e7ac848692
Pramod Yadav <pramodyadavpy2711@gmail.com>
2023-08-01 19:40:41 +0530

    used children props for making reusable button

--- src/App-v1.js
+++ src/App-v1.js
@@ -43,27 +43,21 @@ export default function App() {
           </p>
 
           <div className="buttons">
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
-              <span>👈</span>Previous
-            </Button>
-            <Button textColor="#fff" bgColor="#7950f2" onClick={handleNext}>
-              Next<span>👉</span>
-            </Button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handlePrevious}
+            >
+              Previous
+            </button>
+            <button
+              style={{ backgroundColor: "#7950f2", color: "#fff" }}
+              onClick={handleNext}
+            >
+              Next
+            </button>
           </div>
         </div>
       )}
     </>
-  );
-}
-// used children props to pass vales between the opening and closing tags of Button Components above
-// "children" is a predefined keyword in react for passing children props
-function Button({ textColor, bgColor, onClick, children }) {
-  return (
-    <button
-      style={{ backgroundColor: bgColor, color: textColor }}
-      onClick={onClick}
-    >
-      {children}
-    </button>
   );
 }
