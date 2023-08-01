56a0cc3b1bff0b5faa52d3254511fc9be167166b
Pramod Yadav <pramodyadavpy2711@gmail.com>
2023-08-01 19:53:55 +0530

    made message component reusable

--- src/App.js
+++ src/App.js
@@ -38,9 +38,7 @@ export default function App() {
             <div className={step >= 3 ? "active" : ""}>3</div>
           </div>
 
-          <p className="message">
-            Step {step} : {messages[step - 1]}
-          </p>
+          <StepMessage step={step}> {messages[step - 1]} </StepMessage>
 
           <div className="buttons">
             <Button textColor="#fff" bgColor="#7950f2" onClick={handlePrevious}>
@@ -65,5 +63,14 @@ function Button({ textColor, bgColor, onClick, children }) {
     >
       {children}
     </button>
+  );
+}
+
+function StepMessage({ step, children }) {
+  return (
+    <div className="message">
+      <h3>Step {step} :</h3>
+      {children}
+    </div>
   );
 }
